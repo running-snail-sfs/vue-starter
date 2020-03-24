bef5576fcea536986a306265f6b7fc9f4a1bd710
unknown <742410510@qq.com>
2020-03-24 10:29:08 +0800

    all

---
+++ src/views/login/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      ref="loginForm"
+      label-width="100px"
+      class="login-ruleForm"
+    >
+      <el-form-item label="用户名" prop="userName">
+        <el-input
+          type="text"
+          v-model="ruleForm.userName"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="passWord">
+        <el-input
+          type="password"
+          v-model="ruleForm.passWord"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('loginForm')"
+          >提交</el-button
+        >
+        <el-button @click="resetForm('loginForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        userName: "",
+        passWord: ""
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        console.log("loginForm---", valid);
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+<style lang="less" scoped src="./index.less"></style>
